b673432f04abcb12d232f2185a60bcde653a2e59
Alexander <alexandre.bobkov@gmail.com>
2020-10-17 12:15:51 -0400

    image

--- lib/main.dart
+++ lib/main.dart
@@ -50,15 +50,24 @@ class AboutPage extends StatelessWidget {
 class ProjectsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /*return Container (
+      child: Row (
+        children: <Widget> [
+          Image.asset('assets/images/20200916_132932.jpg'),
+        ],
+      ),
+    );*/
     return Scaffold (
       appBar: AppBar(title: Text("PROJECTS")),
       body: Center(
-        child: Text("Bobkov Estate Improvement Ltd.\n\n"
+        child:
+        /*Text("Bobkov Estate Improvement Ltd.\n\n"
             "M060 - 1554 Carling Ave.\n"
             "Ottawa ON K1Z 7M4\n\n"
             "(819) 639 - 4478\n"
             "bobkovestateimprovement@gmx.com",
-            textAlign: TextAlign.center),
+            textAlign: TextAlign.center),*/
+        Image.asset('assets/images/20200916_132932.jpg'),
       ),
     );
   }
